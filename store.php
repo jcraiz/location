9528ccccb2bd83e8a2a47fd42a93eba9f32cb976
jcraiz <63567373+jcraiz@users.noreply.github.com>
2024-08-18 14:10:25 -0500

    Delete store.php

--- store.php
+++
@@ -1,6 +0,0 @@
-<?php
-$myfile = fopen("location.txt", "w") or die("Unable to open file!");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"];
-fwrite($myfile, $txt);
-fclose($myfile);
-?>
